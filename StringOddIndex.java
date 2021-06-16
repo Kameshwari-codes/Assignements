8837f8be603abaeecd7eb523f6bbf584707507db
Kameshwari-codes <85889407+Kameshwari-codes@users.noreply.github.com>
2021-06-16 22:45:43 +0530

    Update StringOddIndex.java

--- StringOddIndex.java
+++ StringOddIndex.java
@@ -3,7 +3,7 @@ package week1.Assignments;
 public class StringOddIndex {
 
 	public static void main(String[] args) {
-		String str = "Kameshwari";
+		String str = "kameshwari";
 		System.out.println("Length of string" + " " + str.length());
 		char[] x = str.toCharArray();
 		for (int i = 0; i <= x.length - 1; i++) {
